ee8d11dab2f4891573eff5c59c8af18c47a0b951
Alvin Cheng <alvincheng50@gmail.com>
2021-03-28 18:56:57 -0400

    added functionality for connecting to production or test db

---
+++ server/dbHandler.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const { MongoURI } = require('../config.js');
+
+const mongod = new MongoMemoryServer();
+
+/**
+ * Connect to MongoMemoryServer db if testing, else connect to production db
+ */
+module.exports.connect = async () => {
+  const mongooseOpts = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+  let activeURI;
+
+  if (process.env.NODE_ENV === 'test') {
+    const testDbURI = await mongod.getUri();
+    activeURI = testDbURI;
+  } else {
+    activeURI = MongoURI;
+  }
+
+  await mongoose.connect(activeURI, mongooseOpts);
+};
+
+/**
+ * Used only for test db. Removes all documents from all collections in db
+ */
+module.exports.clearDatabase = async () => {
+  if (process.env.NODE_ENV === 'test') {
+    const { collections } = mongoose.connection;
+    const results = [];
+
+    Object.values(collections).forEach((collection) => {
+      results.push(collection.deleteMany());
+    });
+
+    await Promise.all(results);
+  }
+};
+
+/**
+ * Used only for test db. Drop db, close the connection, and stop mongod
+ */
+module.exports.closeDatabase = async () => {
+  if (process.env.NODE_ENV === 'test') {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+    await mongod.stop();
+  }
+};
