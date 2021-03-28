fe512b444363460a25a7bada47e33f9201a3f5b4
Alvin Cheng <alvincheng50@gmail.com>
2021-03-28 16:31:41 -0400

    boilerplate

---
+++ config.example.js
@@ -0,0 +1,5 @@
+const mongoURI = '';
+
+module.exports = {
+  mongoURI
+};
---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const cleanWpPi = require('clean-webpack-plugin');
+const htmlWpPi = require('html-webpack-plugin');
+const miniCssExPi = require('mini-css-extract-plugin');
+const cssMiniWpPi = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    // [contenthash] will be replaced with a hash that webpack generates based on the content of the code (useful for cache busting)
+    filename: 'bundle.[contenthash].js',
+    path: path.join(__dirname, 'build'),
+    publicPath: '/',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'client'),
+    hot: true,
+    // match the output's 'publicPath'
+    publicPath: '/',
+    proxy: {
+      '/': 'http://localhost:3000',
+    },
+  },
+  mode: process.env.NODE_ENV,
+  optimization: {
+    minimizer: [
+      // for webpack@5 can use '...' to extend existing minimizers (i.e. `terser-webpack-plugin` for JS minification)
+      '...',
+      new cssMiniWpPi()
+    ]
+  },
+  plugins: [
+    new cleanWpPi(),
+    new htmlWpPi({
+      template: './client/index.html'
+    }),
+    new miniCssExPi({
+      filename: '[name].[contenthash].[ext]'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        use: 'html-loader',
+      },
+      {
+        test: /\.(css|scss)$/,
+        exclude: /node_modules/,
+        use: [
+          process.env.NODE_ENV === 'production' ? miniCssExPi.loader : "style-loader",
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      // {
+      //   test: /\.(png|jpg|jpeg)$/,
+      //   use: {
+      //     loader: 'file-loader',
+      //     options: {
+      //       name: '[name].[hash].[ext]',
+      //       outputPath: "images"
+      //     }
+      //   }
+      // }
+    ],
+  },
+};
