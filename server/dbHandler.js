ce424f77f9f121676dbea177153ab21a387077da
Alvin Cheng <alvincheng50@gmail.com>
2021-03-28 19:19:44 -0400

    added eslint disable

--- server/dbHandler.js
+++ server/dbHandler.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+// eslint-disable-next-line import/no-extraneous-dependencies
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const { MongoURI } = require('../config.js');
 
