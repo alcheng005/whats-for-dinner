640e922cad4a959a1bc9b2b92ad2890e3fdd63af
Alvin Cheng <alvincheng50@gmail.com>
2021-03-31 18:31:39 -0400

    added room model

---
+++ server/models/roomModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const roomSchema = new Schema({
+  code: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 1800, // 30 mins
+  },
+});
+
+module.exports = mongoose.model('room', roomSchema);
