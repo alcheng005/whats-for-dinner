a8f07a1621903a7b45c5730045270d03c3a1d51f
Alvin Cheng <alvincheng50@gmail.com>
2021-03-31 00:45:48 -0400

    added basic room route component for testing

---
+++ client/components/Room.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Room extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { id } = this.props.match.params;
+
+    return (
+      <section id="roompage">
+        <header>
+          <h2>Room: {id}</h2>
+        </header>
+      </section>
+    );
+  }
+}
+
+{/* <div id="roompage">
+<header>
+  <h2>Room: FAKE</h2>
+</header>
+<section id="add-remove-buttons">
+  <div id="add-button">
+    <button onClick={this.handleAdd}>Add item</button>
+    <input type='text' id='addItem' />
+  </div>
+  <div id="remove-button">
+    <button onClick={this.handleRemove}>Remove item</button>
+    <input type='text' id='removeItem' />
+  </div>
+</section>
+<h3>Your Plate of Food:</h3>
+<div id="room-loc-list">
+  {locList}
+</div>
+{this.state.numItems > 0 && 
+  <Link to="/room/vote">
+    <button id="go-to-vote-button">Take Us to Vote</button>
+  </Link>
+}
+</div> */}
+
+export default Room;
