5b1a27d98253dd7facd172b499a8279ac8b581a8
Alvin Cheng <alvincheng50@gmail.com>
2021-04-06 17:16:50 -0400

    edited routers for clarity

--- server/routers/roomRouter.js
+++ server/routers/roomRouter.js
@@ -9,25 +9,47 @@ const webpackDevConfig = require('../../webpack.dev.js');
 
 const router = express.Router();
 
-router.post('/create-room',
+router.get('/create-room',
   roomController.createRoom,
+  roomController.verifyRoom,
   (req, res) => {
-    res.status(302).redirect(`/room/${res.locals.roomCode}`);
+    console.log('using /create-room prod');
+    res.status(200).json({ roomCode: res.locals.roomCode });
   },
 );
 
-if (process.env.NODE_ENV === 'production') {
-  router.get('/:roomId',
-    roomController.verifyRoom,
-    (req, res) => {
-      res.status(200).sendFile(path.join(__dirname, '../../build/index.html'));
-    },
-  );
-} else {
-  router.get('/:roomId',
-    roomController.verifyRoom,
-    webpackDevMw(webpack(webpackDevConfig)),
-  );
-}
+router.post('/join-room',
+  roomController.verifyRoom,
+  (req, res) => {
+    res.status(200).json({ roomCode: res.locals.roomCode });
+  },
+);
+
+// if (process.env.NODE_ENV === 'production') {
+router.get('/:roomCode',
+  roomController.verifyRoom,
+  (req, res) => {
+    console.log('using /:roomCode prod for:', req.params);
+    console.log('res.locals.roomCode:', res.locals.roomCode);
+    if (res.locals.roomCode !== null) {
+      return res.status(200).sendFile(path.join(__dirname, '../../build/index.html'));
+    }
+
+    return res.status(302).redirect('..');
+  },
+);
+// } else {
+//   router.get('/:roomCode',
+//     roomController.verifyRoom,
+//     (req, res, next) => {
+//       console.log('yoyoyoo');
+//       if (res.locals.roomExists) return next();
+
+//       console.log('wuh oh');
+//       return res.status(302).redirect('/');
+//     },
+//     webpackDevMw(webpack(webpackDevConfig)),
+//   );
+// }
 
 module.exports = router;
