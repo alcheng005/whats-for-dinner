b49f009544dcd6e957c6bd72aaa55225557c2c56
Alvin Cheng <alvincheng50@gmail.com>
2021-04-10 21:42:00 -0400

    edited Room schema

--- server/models/roomModel.js
+++ server/models/roomModel.js
@@ -7,11 +7,17 @@ const roomSchema = new Schema({
     type: String,
     required: true,
     unique: true,
+    uppercase: true,
   },
   createdAt: {
     type: Date,
     default: Date.now,
     expires: 1800, // 30 mins
+  },
+  phase: {
+    type: String,
+    enum: ['menu', 'vote', 'results'],
+    default: 'menu',
   },
   menuOptions: Schema.Types.Mixed,
 });
