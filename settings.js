391eda58bb436835bafe89433e5fb357ff0dd2f5
Alvin Cheng <alvincheng50@gmail.com>
2021-04-17 14:01:30 -0400

    broke config file up into settings and settings.secret files

--- settings.js
+++ settings.js
@@ -4,11 +4,7 @@ require('regenerator-runtime/runtime'); // for async/await in testing
 const validRoomChars = 'BCDFGHJKLMNPQRSTVWXZ';
 const roomCodeLength = 4;
 
-// change value of mongoURI to your mongoDB URI
-const mongoURI = 'your-mongoDB-URI-here';
-
 module.exports = {
   validRoomChars,
   roomCodeLength,
-  mongoURI,
 };
---
+++ settings.secret.example.js
@@ -0,0 +1,6 @@
+// change value of mongoURI to your mongoDB URI
+const mongoURI = 'your-mongoDB-URI-here';
+
+module.exports = {
+  mongoURI,
+};
